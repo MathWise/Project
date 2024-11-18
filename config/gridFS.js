5e061b39c36c0fc434d906c76c8f167f4d8a7d32
DarkBlaak <ronelmagbujos2@gmail.com>
2024-11-18 22:17:49 +0800

    updated lesson section
    nov 18

--- config/gridFS.js
+++ config/gridFS.js
@@ -1,4 +1,3 @@
-// config/gridFS.js
 const { MongoClient, GridFSBucket } = require('mongodb');
 const mongoURI = process.env.MONGODB_URI;
 
@@ -6,14 +5,32 @@ let pdfBucket;
 let videoBucket;
 
 const initBuckets = async () => {
-    const client = await MongoClient.connect(mongoURI);
-    const db = client.db();
-    pdfBucket = new GridFSBucket(db, { bucketName: 'pdfs' });
-    videoBucket = new GridFSBucket(db, { bucketName: 'videos' });
-    console.log("Initialized PDF and Video Buckets");
+    try {
+        const client = await MongoClient.connect(mongoURI); 
+        const db = client.db();
+
+        pdfBucket = new GridFSBucket(db, { bucketName: 'pdfs' });
+        videoBucket = new GridFSBucket(db, { bucketName: 'videos' });
+
+        console.log("GridFS Buckets Initialized: PDFs and Videos");
+    } catch (error) {
+        console.error("Error initializing GridFS Buckets:", error);
+        throw error;
+    }
 };
 
-const getPdfBucket = () => pdfBucket;
-const getVideoBucket = () => videoBucket;
+const getPdfBucket = () => {
+    if (!pdfBucket) {
+        throw new Error("PDF Bucket not initialized. Call initBuckets() first.");
+    }
+    return pdfBucket;
+};
+
+const getVideoBucket = () => {
+    if (!videoBucket) {
+        throw new Error("Video Bucket not initialized. Call initBuckets() first.");
+    }
+    return videoBucket;
+};
 
 module.exports = { initBuckets, getPdfBucket, getVideoBucket };
